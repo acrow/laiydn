d5e52a5b605733bad7a34b8cf617600911df5971
acrow <zhengchunchen@live.cn>
2015-06-10 22:58:43 +0800

    微信接口

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,11 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
+router.get('/weixin', function(req, res, next) {
+	var echostr = req.param('echostr')
+	console.log(echostr);
+	res.send(echostr);
+
+});
 
 module.exports = router;
