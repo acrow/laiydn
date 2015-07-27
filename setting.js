dd3193c6d6edfaa9b5fc0dad849c725e1de3d1df
ZhengChunChen <acrow@sohu.com>
2015-07-27 11:01:11 +0800

    weixin

--- setting.js
+++ setting.js
@@ -1,12 +1,12 @@
 module.exports = {
 	db					: 'laiyd',
-	// host				: 'www.laiyd.cn',						// 正式
-	host				: 'www.laiyd.cn',						// 测试
+	host				: 'www.laiyd.cn',						// 正式
+	// host				: 'www.laiyd.com',						// 测试
 	dbHost				: 'localhost',
-	// weixinAppId			: 'wx4dcce61ef82e8cfb',				// 正式
-	// weixinSecret		: '07a2def88704289da6f7aac5fe6135d2',	// 正式
-	weixinAppId			: 'wxdd46622ce2a78fee',					// 测试
-	weixinSecret		: '1e5ba02ed46f630c99e9631403e67aad',	// 测试
+	weixinAppId			: 'wx4dcce61ef82e8cfb',				// 正式
+	weixinSecret		: '07a2def88704289da6f7aac5fe6135d2',	// 正式
+	// weixinAppId			: 'wxdd46622ce2a78fee',					// 测试
+	// weixinSecret		: '1e5ba02ed46f630c99e9631403e67aad',	// 测试
 	weixinUserToken		: 'albbh40dd',    						//token 与公众号设置需要保持一致
 	cookieSecret		: 'acrow',
 	cookieMaxAge		: 8000000
